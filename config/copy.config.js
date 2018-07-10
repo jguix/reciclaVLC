36a8cc37f37e2c60841d8388d9fb6b97dd48dd08
Juangui Jordán <juangui.jordan@mimacom.com>
2018-07-10 08:16:22 +0200

    Added link

--- config/copy.config.js
+++ config/copy.config.js
@@ -1,6 +1,8 @@
 /**
  * Custom copy config, needed to copy custom components
  * The component lib installed under assets/lib is not automatically included in the vendor.js file
+ * See 
+ * https://github.com/Aaron-Sterling/time-ago-web-component/blob/master/docs/ionic-angular-setup.md
  * See https://forum.ionicframework.com/t/example-integration-stenciljs-web-component-in-ionic-app/106083/5
  */
 module.exports = {
